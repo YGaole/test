acaf4f91e5e2e5cffb8c8ec71bbcf8dc32e177f7
YGaole <rt12366@gmail.com>
2018-09-16 18:07:07 +0800

    Do some corrects.

--- first.py
+++ first.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as mlp
 a,b,c=1,-1,-1
-d1=-b+np.sqrt(b**2-4*a*c)/2/a
-d2=-b-np.sqrt(b**2-4*a*c)/2/a
+d1=(-b+np.sqrt(b**2-4*a*c))/2/a
+d2=(-b-np.sqrt(b**2-4*a*c))/2/a
+print(a*d1**2+b*d1+c);
+print(a*d2**2+b*d2+c)
